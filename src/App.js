899ece6016f28757f91c3aee6c7d4d3c9c11f8f0
jacksonbrunoss <oi.jbss@gmail.com>
2019-07-28 15:25:36 -0300

    Modificações

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="app-title">octal<span>bit</span></h1>
-        <p>Desenvolvimento de sites, sistemas e aplicativos.</p>
+        <p className="app-subtitle">Desenvolvimento de sites, sistemas e aplicativos.</p>
       </header>
     </div>
   );
